f2dcdce80caf227fdb3a512eaff584af7f6bddb2
Yang58 <lloesmp@naver.com>
2023-01-31 13:45:24 +0900

    ttttttt

--- Project_sub/src/main/java/org/zerock/controller/AdminController.java
+++ Project_sub/src/main/java/org/zerock/controller/AdminController.java
@@ -1,63 +1,106 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.AdminBoardDTO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.PageDTO;
+import org.zerock.service.AdminBoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Controller
+@Log4j
 @RequestMapping("/admin/*")
-public class AdminController {
+@AllArgsConstructor
+public class AdminBoardController {
 
-	@RequestMapping("/home")
-	public String main() {
-		// 관리자 계정 메인 
+	private AdminBoardService service;
+	
+	@GetMapping("/home")
+	public String goHome() {
 		return "/admin/main";
 	}
-	@RequestMapping("freeboard")
-	public String freeboard() {
-		// 자유 게시판 관리
-		return "/admin/management/freeboard";
-	}
-	@RequestMapping("club_Applist")
-	public String applist() {
-		// 자유 게시판 관리
-		return "/admin/management/club_Applist";
-	}
-	@RequestMapping("qnaboard")
-	public String qnaboard() {
-		// 문의 게시판 관리
-		return "/admin/management/qnaboard";
-	}
-	@RequestMapping("member_registration")
-	public String member_registration() {
-		// 회원 등록
-		return "/admin/management/member_registration";
-	}
-	@RequestMapping("member_management")
-	public String member_management() {
-		// 회원 관리 
-		return "/admin/management/member_management";
-	}
-	@RequestMapping("club_registration")
-	public String club_registration() {
-		// 동호회 등록 
-		return "/admin/management/club_registration";
-	}
-	@RequestMapping("club_management")
-	public String club_management() {
-		// 동호회 관리 
-		return "/admin/management/club_management";
-	}
-	
-	@RequestMapping("create_Announcement")
-	public String create_Announcement() {
-		// 공지사항 작성 
-		return "/admin/management/create_Announcement";
-	}
-	
-	@RequestMapping("clubdetail")
-	public String clubdetail() {
-		// 공지사항 작성 
-		return "/admin/management/club_detail";
+
+	@GetMapping("/write")
+	public String write() {
+		log.info("controller write Page Move .... ");
+		return "/admin/board/Announcement_write";
 	}
 
+	@PostMapping("/write")
+	public String postWrite(AdminBoardDTO dto) throws Exception {
+		log.info("controller write run .... ");
+		service.write(dto);
+		return "redirect:/admin/list";
+	}
+	
+	@GetMapping("/list")
+	public String list (Criteria cri , Model model ) {
+		log.info("list :" + cri);
+		model.addAttribute("list", service.getList(cri));
+		model.addAttribute("aanoList", service.getList());
+		log.info("list : " + service.getList(cri));
+		log.info("aanoList : " + service.getList());
+		int total = service.getTotal(cri);
+		log.info("total : " + total);
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+
+		return "/admin/board/freeboard";
+		
+	}
+
+
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public String getRead(@RequestParam("bno") Long bno, Model model) {
+		log.info("Get Read") ;
+		AdminBoardDTO dto = service.read(bno);
+
+		model.addAttribute("read", dto);
+
+		return "/admin/board/FreeBoardDetail";
+	}
+
+	// 글 수 정
+	@GetMapping(value = "/modify")
+	public String getModify(@RequestParam("bno") Long bno, Model model) {
+		log.info("get modify");
+
+		AdminBoardDTO vo = service.read(bno);
+
+		model.addAttribute("modify", vo);
+
+		return "/admin/board/Announcement_modify";
+	}
+
+	// 글 수정 POST
+	@PostMapping("/modify")
+	public String postModify(AdminBoardDTO vo) {
+		log.info("post modify");
+
+		service.update(vo);
+
+		return "redirect:/admin/list";
+
+	}
+
+	// 글 삭제 POST
+	@PostMapping(value = "/delete")
+	public String postDelete(@RequestParam("bno") Long bno) {
+		log.info("post delete");
+
+		service.delete(bno);
+
+		return "redirect:/admin/list";
+	}
+	
+	
 }
